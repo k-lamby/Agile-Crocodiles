123d7fef8528da1cbb6ae52b22f8dca1dc3b658b
lordlamberino <58035625+lordlamberino@users.noreply.github.com>
2021-06-10 13:38:12 +0100

    Database connection

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,16 @@ module.exports = function(app) {
 
     //route for the main landing page, renders the index view
     app.get("/", function(req, res) {
-        res.render("index.html")
+
+        let sqlquery = "SELECT * FROM users"
+
+        db.query(sqlquery, (error, results, fields) => {
+
+            let user = results[0];
+            
+            res.render("index.html", { user });  
+        });
+
     });
 
 }
