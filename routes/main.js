3a10ff0a0a2357864e2e10b896cbca2ce5f4b8fb
holyjc827 <holyjc827@gmail.com>
2021-07-28 19:38:16 +0200

    1) Middleware logic : Session message is added. 2) One of the attributes in FE is changed for body-parser

--- routes/main.js
+++ routes/main.js
@@ -25,6 +25,38 @@ module.exports = function(app){
     })
 
     app.post("/register/new-user",function(req, res){
+        if(req.body.registerPassword === req.body.registerPasswordConfirm) // check if password and password confirm are the same
+        {
+            let query = "INSERT INTO credential (userID, password) VALUES ('" + req.body.registerUsername + "','" + req.body.registerPassword + "');"
+            connection.query(query, (err, results)=>{
+                if(err) // Since USER ID is set to "UNIQUE", an exception will be thrown if the same userID already exists. 
+                {
+                    req.session.message = {
+                        type: 'danger',
+                        intro: 'Username Conflict!',
+                        message: 'The same username is already being used by another user. Please choose another username.'
+                    }
+                    res.redirect('/register');
+                } 
+                // if there is no duplicate USER ID in the database
+                else{
+                    req.session.message = {
+                        type: 'success',
+                        intro: 'Registration successful!',
+                        message: 'You are successfully registered. Please log in.'
+                    }
+                    res.redirect('/register');
+                }
+            })
+        }
+        else{ // Passwords do not match
+            req.session.message = {
+                type: 'danger',
+                intro: 'Unmatched passwords',
+                message: 'The provided passwords do not match.'
+            }
+            res.redirect('/register');
+        }
         console.log("A post request was made to /register/new-user");
     })
 }
