77eb3bfce26b48803ae3d946555f36a5e22e4823
holyjc827 <holyjc827@gmail.com>
2021-08-02 13:44:43 +0200

    Unit test for iteration 1 is implemented.

---
+++ test/test.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+require('../main');
+
+describe('iteration1', () =>{
+    it('Can access index.ejs', (done) => {
+        chai.request('http://0.0.0.0:3000/')
+            .get("")
+            .end((err, res) =>{
+                expect(res.statusCode).to.equal(200);
+                done();
+            })
+
+    })
+
+    it('Can post user information for registration', (done) => {
+        let js = {
+            registerUsername : 'user1',
+            registerPassword : 'xx',
+            registerPasswordConfirm : 'xx'
+        }
+        chai.request('http://0.0.0.0:3000/')
+            .post('register/new-user')
+            .send(js)
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200);
+                expect(res).to.have.property('body');
+                done();
+            })
+    })
+
+    it('Can remove a book from the wish list', (done) => {
+        chai.request('http://0.0.0.0:3000/')
+            .post('wishlist/remove')
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200);
+                done();
+            })
+    })
+
+})
