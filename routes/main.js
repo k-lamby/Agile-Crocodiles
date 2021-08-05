1858ebb96d6c54e26386fd4ffd250142315846c0
holyjc827 <holyjc827@gmail.com>
2021-08-05 10:33:52 +0200

    1) Profile page : checkbox is implemented. 2) main.js : middleware logic to handle the user genre preference has been implemented.

--- routes/main.js
+++ routes/main.js
@@ -1,26 +1,69 @@
-module.exports = function(app){
+module.exports = (app) => {
     let bookInfo = [] // To store json objects for books in the wish list.
     queryWishlist(); // Get the wish list beforehands.
 
-    app.get("/",function(req, res){
-        res.render("index.ejs", {title: "Main"})
+    app.get("/",(req, res) => {
+        res.render("index.ejs", {title: "Main"});
     })
 
-    app.get("/match",function(req, res){
-        res.render("match.ejs", {title: "Match"})
+    app.get("/match",(req, res) => {
+        res.render("match.ejs", {title: "Match"});
     })
 
-    app.get("/profile",function(req, res){
-        res.render("profile.ejs", {title: "Profile"})
+    app.get("/profile",(req, res) => {
+        let query = ["SELECT preference FROM userProfile WHERE userIDNum = 9"]
+        connection.query(query.join(";"), (err, results) => {
+            if(err) throw err;
+            genreList = results[0].preference.split(";"); //tokenize the user preference string by ";"
+            genreList.pop(); // remove the last element in the array which is an empty string.
+            res.render("profile.ejs", {title: "Profile", genreList : genreList});
+        });
     })
 
-    app.get("/wishlist", function(req, res){
+    app.post("/profile", (req, res) => {
+        let preference = savePreference(req.body.genre);
+        let query = ["INSERT INTO userProfile (userIDNum, preference) VALUES (9, " + preference + ")"];
+        connection.query(query.join(";"), (err, results) => {
+            if (err) // If INSERT fails, then there is already a saved preference for this user. So, UPDATE is required.
+            {
+                let query = ["UPDATE userProfile SET preference = '" + preference + "' WHERE USERIDNum = 9"];
+                connection.query(query.join(";"), (err, results) => {
+                    if(err) throw err;
+                    console.log("User preference has been saved.")
+                });
+            }
+            else {
+                res.render("profile.ejs", {title: "Profile"}); // INSERT is successful.
+            }
+        })
+        res.render("profile.ejs", {title: "Profile"}); // UPDATE is successful.
+    });
+
+    //savePreference function processes the user's genre choices by concatenating them in a one single string by semi-colon
+    // and save it to the DB. 
+    function savePreference(input) 
+    {
+        if(typeof input == "string") // Only one genre was selected by the user. 
+        {
+            return input + ";"
+        }
+        else{
+            let preference = ""
+            for(let i = 0; i < input.length; i++)
+            {
+                preference = preference + input[i]+";"
+            }
+            return preference
+        }
+    }
+
+    app.get("/wishlist", (req, res) => {
         var clean = bookInfo.filter((arr, index, self) =>
-        index === self.findIndex((t) => (t.title === arr.title && t.cover === arr.cover))) // remove duplicate json from bookInfo
+        index === self.findIndex((t) => (t.title === arr.title && t.cover === arr.cover))); // remove duplicate json from bookInfo
         res.render("wishlist.ejs", {title: "Wishlist", bookInfo : clean});
     })
 
-    function formatData(queryResults)
+    function formatData (queryResults)
     {
         let json = {
             title : queryResults[0][0].name,
@@ -52,7 +95,7 @@ module.exports = function(app){
         })
     }
 
-    app.post("/wishlist/remove", function(req, res){
+    app.post("/wishlist/remove", (req, res) => {
         bookInfo.splice(bookInfo.findIndex((element) => element.title == req.body.title), 1); // remove book from the array
         let query = ["DELETE FROM wishlist WHERE titleID = (SELECT ID FROM title WHERE name = '" + req.body.title+"')"];
         connection.query(query.join(';'), (err, results) => {
@@ -62,7 +105,7 @@ module.exports = function(app){
         })
     });
 
-    app.post("/wishlist/oneliner", function(req, res){
+    app.post("/wishlist/oneliner", (req, res) => {
         bookInfo.splice(bookInfo.findIndex((element) => element.title == req.body.title), 1); // remove book from the array
         let query = ["INSERT INTO oneliner VALUES ((SELECT ID FROM title WHERE name = '" + req.body.modalTitle + "'), 9 ,'" + req.body.oneliner+ "');"];
         connection.query(query.join(';'), (err, results) => {
@@ -78,19 +121,19 @@ module.exports = function(app){
         })
     });
 
-    app.get("/register",function(req, res){
+    app.get("/register",(req, res) => {
         res.render("register.ejs", {title: "Registration"})
     })
 
-    app.post("/login",function(req, res){
+    app.post("/login", (req, res)=> {
         console.log("A post request was made to /login");
     })
 
-    app.post("/register/new-user",function(req, res){
+    app.post("/register/new-user", (req, res) => {
         if(req.body.registerPassword === req.body.registerPasswordConfirm) // check if password and password confirm are the same
         {
             let query = "INSERT INTO credential (userID, password) VALUES ('" + req.body.registerUsername + "','" + req.body.registerPassword + "');"
-            connection.query(query, (err, results)=>{
+            connection.query(query, (err, results) =>{
                 if(err) // Since USER ID is set to "UNIQUE", an exception will be thrown if the same userID already exists. 
                 {
                     req.session.message = {
